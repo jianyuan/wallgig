6db1029c900225cb9cafb4e7cd1e5bb5c823e183
Jian Yuan <jianyuan@gmail.com>
2013-12-08 20:46:46 +0000

    Add username color

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,15 @@
 module UsersHelper
   def user_tag(user)
-    link_to user.username, user
+    if user.has_role? :developer
+      css_class = 'user-developer'
+    elsif user.has_role? :admin
+      css_class = 'user-admin'
+    elsif user.has_role? :moderator
+      css_class = 'user-moderator'
+    else
+      css_class = nil
+    end
+ 
+    link_to user.username, user, class: css_class
   end
 end
