e4d4405d7b2c4c3474979c7d397de62e695b4937
jianyuan <jianyuan@gmail.com>
2014-01-07 03:41:29 +0000

    Fix missing strong parameters when signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :username
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
       devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password, :remember_me) }
     end
 
